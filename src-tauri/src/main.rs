9491ca017d669d5fe37970505e29a18e1aa1dcbb
shueja-personal <j.a.shue@outlook.com>
2023-09-25 12:29:12 -0700

    Adding a cli

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -258,6 +258,18 @@ async fn generate_trajectory(path: Vec<ChoreoWaypoint>, config: ChoreoRobotConfi
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_persisted_scope::init())
+        .setup(|app| {
+          match app.get_cli_matches() {
+            // `matches` here is a Struct with { args, subcommand }.
+            // `args` is `HashMap<String, ArgData>` where `ArgData` is a struct with { value, occurrences }.
+            // `subcommand` is `Option<Box<SubcommandMatches>>` where `SubcommandMatches` is a struct with { name, matches }.
+            Ok(matches) => {
+              println!("{:?}", matches)
+            }
+            Err(_) => {}
+          }
+          Ok(())
+        })
         .invoke_handler(tauri::generate_handler![generate_trajectory, expand_fs_scope])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
